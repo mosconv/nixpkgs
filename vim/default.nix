88c8972ceef7e8cd594f10c7f953e1ff1d72e875
Vítor Moscon <vitormr@gmail.com>
2022-02-16 19:14:13 -0300

    Update default.nix

--- vim/default.nix
+++ vim/default.nix
@@ -7,7 +7,7 @@ vim.mkDerivation {
   src = fetchFromGitHub {
     owner = "mosconv";
     repo = "vim";
-    rev = "${version};
+    rev = "${version}";
     sha256 = "sha256:1qs6h44x6bwvysa6343kdx6dflb2zxhp89ycgsx94mhfcvrc0iac";
   };
 }
