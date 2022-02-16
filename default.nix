bdedb215108dddd1e2bc92841eb0b5d22360a2e7
Vítor Moscon <vitormr@gmail.com>
2022-02-16 00:26:24 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,1 +1,13 @@
+{ system ? builtins.currentSystem }:
 
+let
+    pkgs = import <nixpkgs> { inherit system; };
+
+    callPackage = pkgs.lib.callPackageWith (pkgs // self);
+
+    self = {
+        vim = callPackage ./vim {};
+#        ogle = callPackage ./ogle.nix {};
+    };
+in
+self
